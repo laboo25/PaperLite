d31ed8203e28d5180bd60991910ddeab2847b038
lab00 <89362256+laboo25@users.noreply.github.com>
2026-09-01 22:15:36 +0600

    Add files via upload

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .run(tauri::generate_context!())
-        .expect("error while running PaperLite application");
+    paperlite_lib::run();
 }
+
